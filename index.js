388d80ea7754289819b509206d177e3ee33a15a7
Petr Novoselov <novpeter@yandex.ru>
2021-06-06 23:10:43 +0300

    Add boot simulator function

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-// MIT License - Copyright (c) 2020 Stefan Arentz <stefan@devbots.xyz>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-
-const path = require('path');
-const fs = require('fs');
-
 const core = require('@actions/core');
-const artifact = require('@actions/artifact');
 const execa = require('execa');
 
 const { parseDestination, encodeDestinationOption } = require('./destinations');
+const { bootSimulator } = require('./simulators');
 
 
 const getProjectInfo = async ({workspace, project}) => {
@@ -43,7 +19,6 @@ const getProjectInfo = async ({workspace, project}) => {
 
     return JSON.parse(stdout);
 };
-
 
 const testProject = async ({
     workspace, 
@@ -107,6 +82,8 @@ const parseConfiguration = async () => {
         developmentTeam: core.getInput('development-team'),
         resultBundlePath: core.getInput("result-bundle-path"),
         resultBundleName: core.getInput("result-bundle-name"),
+        screenRecordBundlePath: core.getInput("screen-record-bundle-path"),
+        screenRecordBundleName: core.getInput("screen-record-bundle-name"),
     };
 
     if (configuration.destination !== "") {
@@ -123,51 +100,18 @@ const parseConfiguration = async () => {
     return configuration;
 }
 
-
-const archiveResultBundle = async (resultBundlePath) => {
-    const archivePath = resultBundlePath + ".zip";
-
-    const args = [
-        "-c",             // Create an archive at the destination path
-        "-k",             // Create a PKZip archive
-        "--keepParent",   // Embed the parent directory name src in dst_archive.
-        resultBundlePath, // Source
-        archivePath,      // Destination
-    ];
-
-    try {
-        await execa("ditto", args);
-    } catch (error) {
-        core.error(error);
-        return null;
-    }
-
-    return archivePath;
-};
-
-
-const uploadResultBundleArtifact = async (resultBundleArchivePath, resultBundleName) => {
-    const artifactClient = artifact.create()
-    const uploadResult = await artifactClient.uploadArtifact(
-        resultBundleName,
-        [resultBundleArchivePath],
-        path.dirname(resultBundleArchivePath)
-    )
-};
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const main = async () => {
     try {
         const configuration = await parseConfiguration();
+        const destination = configuration.destination;
 
-        // Run xcodebuild test
+        await bootSimulator(destination);
+        await sleep(12000)
         await testProject(configuration);
-
-        // Upload the results bundle as an artifact
-        if (configuration.resultBundlePath !== "" && fs.existsSync(configuration.resultBundlePath)) {
-            const resultBundleArchivePath = await archiveResultBundle(configuration.resultBundlePath);
-            await uploadResultBundleArtifact(resultBundleArchivePath, configuration.resultBundleName);
-        }
     } catch (err) {
         core.setFailed(`Testing failed with an unexpected error: ${err.message}`);
     }
---
+++ simulators.js
@@ -0,0 +1,78 @@
+const execa = require('execa');
+const core = require('@actions/core');
+
+/**
+ * Transforms given destination string into JSON object
+ * 
+ * ### Example
+ * Given `platform=iOS Simulator,name=iPhone 11 Pro,OS=14.5`
+ * Returns:
+ * ```
+ * {
+ *  'platform': 'iOS Simulator',
+ *  'name': 'iPhone 11 Pro',
+ *  'OS': '14-5'
+ * }
+ * ```
+ * 
+ * @param {*} destination String with params of simulator
+ * @returns JSON object
+ */
+const parseDestination = (destination) => {
+    let splitted = destination.split(',')
+    let destinationJSON = {}
+    
+    splitted.forEach(element => {
+        let pair = element.split('=')
+        let key = String(pair[0])
+        let value = pair[1].replaceAll('.', '-')
+        destinationJSON[key] = value
+    });
+
+    return destinationJSON
+}
+
+/**
+ * Return device UDID with given OS and name
+ * 
+ * @param {String} devices List of devices with specific OS
+ * @param {String} os Reqiured OS
+ * @param {String} simulatorName Required simlator name
+ * @returns 
+ */
+const findDeviceUDID = (devices, os, simulatorName) => {
+    var udid = ''
+    for (key in devices) {
+        if (key.endsWith('iOS-'+os)) {
+            let simulatorsWithOS = devices[key]
+            simulatorsWithOS.forEach(function(simulator, i, arr) {
+                if (simulator['name'] == simulatorName) {
+                    udid = simulator['udid'] 
+                    core.info(`Did found simlator with UDID ${udid}`)
+                } 
+            })   
+        }
+    }
+    return udid
+}
+
+/**
+ * Boot simulator with given destination
+ * 
+ * @param {String} destination String with params of simulator
+ */
+const bootSimulator = async (destination) => {
+    const xcrunDevices = execa('xcrun', ['simctl','list', '-j', '-v', 'devices']);
+    const { stdout } = await xcrunDevices
+    
+    let destinationJSON = parseDestination(destination)
+    let devices = JSON.parse(stdout)['devices']
+    let os = destinationJSON['OS']
+    let udid = findDeviceUDID(devices, os, destinationJSON['name'])
+
+
+    await execa('xcrun', ['simctl', 'boot', udid])
+    await execa('open', ['/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/'])
+};
+
+exports.bootSimulator = bootSimulator;
