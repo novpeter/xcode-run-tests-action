24b8de485f936287cbf8f49b2be4116c949fd555
Petr Novoselov <novpeter@yandex.ru>
2021-06-06 23:34:03 +0300

    Fixed parsing 3

--- simulators.js
+++ simulators.js
@@ -25,7 +25,7 @@ const parseDestination = (destination) => {
     splitted.forEach(element => {
         let pair = element.split('=')
         let key = String(pair[0])
-        let value = String(pair[1]).replaceAll('.', '-')
+        let value = String(pair[1]).replace(/./g, '-');
         destinationJSON[key] = value
     });
 
