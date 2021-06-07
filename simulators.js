75bb81f6cdc069f72f01e5c18ca66c88125eec88
Petr Novoselov <novpeter@yandex.ru>
2021-06-07 06:49:08 +0300

    Replace function

--- simulators.js
+++ simulators.js
@@ -25,7 +25,7 @@ const parseDestination = (destination) => {
     splitted.forEach(element => {
         let pair = element.split('=')
         let key = String(pair[0])
-        let value = String(pair[1]).replace(/./g, '-');
+        let value = String(pair[1]).replace('.', '-');
         core.info(`[${key} : ${value}]`)
         destinationJSON[key] = value
     });
